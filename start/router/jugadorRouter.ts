f394c84a2aa4321bfedebefc02dbbfda717fcff2
yiselorena0808 <yiselorenasalzar@gmail.com>
2025-07-19 02:35:30 -0500

    Update jugadorRouter.ts

--- start/router/jugadorRouter.ts
+++ start/router/jugadorRouter.ts
@@ -3,12 +3,20 @@ import JugadorController from '../../app/controllers/JugadorController.ts'
 
 const jugador = new JugadorController()
 
-Route.get('/jugadores/pais/:id',jugador.listarPorPais)
-Route.get('/jugadores',jugador.listarJugadores)
-Route.post('/jugadores', jugador.crearJugador)
-Route.get('/jugadores/:CodJug',jugador.jugadorPorId)
-Route.put('/jugadores/:CodJug',jugador.actualizarJugador)
-Route.delete('/jugadores/:CodJug',jugador.eliminarJugador)
+Route.get('/jugadores',jugador.listarJugadores);
+Route.post('/jugadores', jugador.crearJugador);
+Route.get('/jugadores/:CodJug',jugador.jugadorPorId);
+Route.put('/jugadores/:CodJug',jugador.actualizarJugador);
+Route.delete('/jugadores/:CodJug',jugador.eliminarJugador);
+Route.get('/jugadores/pais/:id',jugador.listarPorPais);
+Route.get('/jugadores/detallado', jugador.listarConClubYPais);
+Route.get('/jugadores/estadisticas',jugador.estadisticasPorClub);
+Route.get('/jugadores/relaciones',jugador.listarConRelaciones);
+Route.get('/jugadores/nombre-dorsal',jugador.jugadoresNombreYDorsal);
+Route.get('jugadores/paginado',jugador.paginacion);
+
+
+
 
 
 
